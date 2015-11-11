14ef1fdfe4fed89615f41a470abeb4c523381e50
Taylor Blau <me@ttaylorr.com>
2015-11-10 19:30:41 -0500

    PacketIdent: use generic Message class over GeneratedMessage
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- src/main/java/pro/beam/interactive/net/PacketIdentifier.java
+++ src/main/java/pro/beam/interactive/net/PacketIdentifier.java
@@ -1,14 +1,14 @@
 package pro.beam.interactive.net;
 
 import com.google.common.collect.ImmutableBiMap;
-import com.google.protobuf.GeneratedMessage;
+import com.google.protobuf.Message;
 
 public final class PacketIdentifier {
     // Don't instantiate meeee!
     private PacketIdentifier() { }
 
-    private static final ImmutableBiMap<Integer, Class<? extends GeneratedMessage>> PACKETS =
-        ImmutableBiMap.<Integer, Class<? extends GeneratedMessage>>builder()
+    private static final ImmutableBiMap<Integer, Class<? extends Message>> PACKETS =
+        ImmutableBiMap.<Integer, Class<? extends Message>>builder()
             .put(0, Protocol.Handshake.class)
             .put(1, Protocol.HandshakeACK.class)
             .put(2, Protocol.Report.class)
@@ -16,15 +16,15 @@ public final class PacketIdentifier {
             .put(4, Protocol.ProgressUpdate.class)
         .build();
 
-    public static Class<? extends GeneratedMessage> fromId(int id) {
+    public static Class<? extends Message> fromId(int id) {
         return PACKETS.get(id);
     }
 
-    public static <T extends GeneratedMessage> int fromInstance(T message) {
+    public static <T extends Message> int fromInstance(T message) {
         return fromClass(message.getClass());
     }
 
-    public static int fromClass(Class<? extends GeneratedMessage> clazz) {
+    public static int fromClass(Class<? extends Message> clazz) {
         return PACKETS.inverse().get(clazz);
     }
 }
