de536843065b2a2b505c91b9577703a2082faf3b
Taylor Blau <me@ttaylorr.com>
2015-11-10 19:35:27 -0500

    net: encoding and sending messages
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- src/main/java/pro/beam/interactive/robot/Robot.java
+++ src/main/java/pro/beam/interactive/robot/Robot.java
@@ -1,21 +1,26 @@
 package pro.beam.interactive.robot;
 
 import com.google.common.eventbus.EventBus;
+import com.google.protobuf.Message;
 import pro.beam.interactive.net.Decoder;
+import pro.beam.interactive.net.Encoder;
 
 import java.io.IOException;
 import java.net.Socket;
 
 public class Robot {
-    protected final EventBus eventBus;
     protected final Socket socket;
     protected final String authkey;
+
+    protected final EventBus eventBus;
+    protected final Encoder encoder;
 
     public Robot(Socket socket, String authkey) throws IOException {
         this.socket = socket;
         this.authkey = authkey;
 
         this.eventBus = new EventBus(String.format("robot-%d", this.hashCode()));
+        this.encoder = new Encoder(this.socket.getOutputStream());
     }
 
     public void register(Object listenerObject) {
@@ -24,6 +29,10 @@ public class Robot {
 
     public void unregister(Object listenerObject) {
         this.eventBus.unregister(listenerObject);
+    }
+
+    public void send(Message message) throws IOException {
+        this.encoder.write(message);
     }
 
     private void dispatch() throws IOException {
