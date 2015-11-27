6ff43a62d6c6e9e15d23a6f92ac38b532a2a31fc
Taylor Blau <me@ttaylorr.com>
2015-11-26 21:22:00 -0500

    Annotate the example code
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

---
+++ src/example/java/pro/beam/example/MouseMoveListener.java
@@ -0,0 +1,34 @@
+package pro.beam.example;
+
+import pro.beam.interactive.event.EventListener;
+import pro.beam.interactive.net.packet.Protocol;
+
+import java.awt.*;
+
+// MouseMoveListener is responsible for listening to Report messages and handling them.
+public class MouseMoveListener implements EventListener<Protocol.Report> {
+    // This Robot is an AWT Robot, not a Beam one. It knows how to move the mouse across the screen.
+    protected Robot mouse;
+
+    // Basic no-args constructor, nothing special here...
+    public MouseMoveListener() {
+        try {
+            this.mouse = new Robot();
+        } catch (AWTException ignored) { }
+    }
+
+    // The handler method. This method is invoked when a new Report message is received over the wire.
+    // It takes some information given in the Report message and does something with it (in this case,
+    // it moves the mouse across the screen.
+    @Override public void handle(Protocol.Report report) {
+        // Grab the joystick for each axis, the x and y-axis respectively.
+        Protocol.Report.JoystickInfo joystickX = report.getJoystick(0);
+        Protocol.Report.JoystickInfo joystickY = report.getJoystick(1);
+
+        // Move the mouse to the calculated mean of each of the individual axis respectively.
+        mouse.mouseMove(
+                (int) Math.round(joystickX.getInfo().getMean()),
+                (int) Math.round(joystickY.getInfo().getMean())
+        );
+    }
+}
--- src/example/java/pro/beam/example/TetrisExample.java
+++ src/example/java/pro/beam/example/TetrisExample.java
@@ -4,25 +4,40 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import pro.beam.api.BeamAPI;
+import pro.beam.interactive.net.packet.Protocol;
 import pro.beam.interactive.robot.Robot;
 import pro.beam.interactive.robot.RobotBuilder;
 
 public class TetrisExample {
     public static void main(String[] args) {
+        // Construct an instance of the BeamAPI that will be used for communication with Tetris.
         BeamAPI beam = new BeamAPI();
 
+        // This future will return a Robot that is connected. See usage below for how to add a callback to
+        // a ListenableFuture.
+        //
+        // A RobotBuilder is the entry-point to creating a Robot that you can do things with. It follows
+        // the typical builder-pattern as expressed in Java, and an example usage may be found below:
         ListenableFuture<Robot> future = new RobotBuilder().username("<username>")
                                                            .password("<password>")
                                                            .channel(127)
                                                            .build(beam);
 
+        // This is a rudimentary example of how to listen for a callback on a ListenableFuture.
         Futures.addCallback(future, new FutureCallback<Robot>() {
             @Override public void onSuccess(Robot robot) {
                 // The robot is connected and ready to be used.
+
+                // Set up an event handler to listen to the Report message, and delegate
+                // into the given instance of the MouseMoveListener class.
+                robot.on(Protocol.Report.class, new MouseMoveListener());
             }
 
             @Override public void onFailure(Throwable throwable) {
                 // There was an error connecting to the socket.
+
+                System.err.println("Error experienced in connecting to Beam.");
+                throwable.printStackTrace();
             }
         });
     }
